73581817a438ff8627f319ac84dcb181b75c2414
Mr George <95026513+georgep21@users.noreply.github.com>
2023-09-26 15:24:34 +0100

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <!-- Header -->
+    <div></div>
+    <NuxtPage />
+    <!-- Footer -->
+    <div></div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mt-10 ml-10 text-sky-400">
+    Hello
+  </div>
+</template>
